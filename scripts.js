d9f848ad93805bc65e4a360eb465d84d896bafa1
nohyp3 <84885039+nohyp3@users.noreply.github.com>
2021-12-27 23:40:38 -0500

    updated homepage

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
+class NavBar extends React.Component {
+  render() {
+    return (
+        <div>
+        <nav>
+        <div id="logo">
+        <h1 class="name"><a href="index.html">Business Basics</a></h1>
+        </div>
+        <ul>
+            <li><a href="learn.html">Learn</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+        </nav>
+        </div>
+    );
+  }
+}
 
+const domContainer = document.getElementById('nav');
+ReactDOM.render(<NavBar />, domContainer);
