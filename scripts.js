b2ff687bdc06be5f1e47c82d8fb3d936f3722c3f
nohyp3 <84885039+nohyp3@users.noreply.github.com>
2022-01-09 17:53:56 -0500

    added basic layout to pages

--- scripts.js
+++ scripts.js
@@ -16,25 +16,6 @@ class NavBar extends React.Component {
     );
   }
 }
-class learnNavBar extends React.Component {
-  render() {
-    return (
-        <div>
-        <nav>
-        <div id="logo">
-        <h1 class="name"><a href="/index.html">Business Basics</a></h1>
-        </div>
-        <ul>
-            <li><a href="/learn.html">Learn</a></li>
-            <li><a href="/about.html">About</a></li>
-            <li><a href="/contact.html">Contact</a></li>
-            <li><a href="">Basics</a></li>
-        </ul>
-        </nav>
-        </div>
-    );
-  }
-}
 const domContainer = document.getElementById('nav');
 ReactDOM.render(<NavBar />, domContainer);
 const domContainer2 = document.getElementById('learnNav');
