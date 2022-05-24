76d07ecbd8f17abeb991fb34145ba46417cbd34f
nohyp3 <84885039+nohyp3@users.noreply.github.com>
2022-05-23 22:13:01 -0400

    simple quiz functionality

--- learning/script.js
+++ learning/script.js
@@ -1,4 +1,5 @@
 
+
 class NavBar extends React.Component {
     render() {
       return (
@@ -28,51 +29,65 @@ class NavBar extends React.Component {
 const domContainer2 = document.getElementById('nav');
 ReactDOM.render(<NavBar />, domContainer2);
 
-function Quiz() {
-  var currentQuestion = 0;
-  var showScore = false;
-  var score = 0;
-  var questions = 
-  [
-    {
-      question: "question 1",
-      answers: [{text:"ans1", isCorrect: false}, {text:"ans2", isCorrect: true},{text:"ans3", isCorrect: false},
-      {text:"ans4", isCorrect: false}]
-    },
-    {
-      question: "question 2",
-      answers: [{text:"ans1", isCorrect: false}, {text:"ans2", isCorrect: true},{text:"ans3", isCorrect: false},
-      {text:"ans4", isCorrect: false}]
-    },
-    {
-      question: "question 3",
-      answers: [{text:"ans1", isCorrect: false}, {text:"ans2", isCorrect: true},{text:"ans3", isCorrect: false},
-      {text:"ans4", isCorrect: false}]
-    }
-
-  ]
-  const handleClick = (isCorrect) => {
-    if(isCorrect){
-        alert("correct");
-    }
-    if(currentQuestion + 1 <= questions.length){
-      currentQuestion++;
-    }
-    else {
-      showScore = true;
-    }
-  }
-  return(
-    <div>
-    <h1>{questions[currentQuestion].question}</h1>
-    <h1>Score: {score}</h1>
-    <div className="answerPart">
-      {questions[currentQuestion].answers.map((answers, index) => (
-        <button onClick={() => handleClick(answers.isCorrect)}>{answers.text}</button>
-      ))}
-    </div>
-    </div>
-  )
+class Quiz extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			question: 1,
+			score: 0,
+		}
+		this.getQuestion = this.getQuestion.bind(this);
+		this.getOptions = this.getOptions.bind(this);
+		this.validate = this.validate.bind(this);
+	}
+	getQuestion() {
+		var questions = ["what is a business?", "why are regulations important?", "what is an oliogopoly?", "congrats! check your score!", null]
+		return questions[this.state.question-1]
+	}
+	validate(e) {
+		var correctAnswers = [0,1,2]
+		if(e.target.id == correctAnswers[this.state.question-1]){
+			this.setState(prevState =>{
+				return{
+					question: prevState.question+1,
+					score: prevState.score+1
+				}
+			})
+		}
+		else {
+			this.setState(prevState => {
+				return{
+					question: prevState.question+1
+				}
+			})
+		}
+	}
+	getOptions() {
+		var options = [["an organization or enterprise entity engaged in commercial, industrial, or professional activities", 
+		"a government-owned entity", "a place that sells stuff"],["because they give the government tax revenue","because they help protect the environment, people, companies and more","because they ensure that competition will not be able to compete"],
+		["when a single entity has control over a whole industry", "when there are two entities that have complete control over a industry", "when there are a handful of companies that have complete control over a industry"]
+		]
+		if(this.state.question != 4){
+		return (
+			<div>
+			<button onClick={this.validate} id="0">{options[this.state.question-1][0]}</button>
+			<button onClick={this.validate} id="1">{options[this.state.question-1][1]}</button>
+			<button onClick={this.validate} id="2">{options[this.state.question-1][2]}</button>
+			</div>
+		)
+		}
+	}
+	render() {
+		return(
+			<div>
+			<h2>Quiz</h2>
+			<h3>Score: {this.state.score}</h3>
+			<h3>{this.getQuestion()}</h3>
+			{this.getOptions()}
+			
+			</div>
+		)
+	}
 }
-const element = <Quiz />;
+
 ReactDOM.render(<Quiz />, document.getElementById('quiz'));
